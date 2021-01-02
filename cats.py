dd3b92314ec46991912beb7dff4336c1009b010e
Southpaw1496 <appleauthority@icloud.com>
2021-01-02 16:53:58 +0000

    Add summon command

--- cats.py
+++ cats.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import Embed
+from discord import Embed, Colour
 from os import environ
 import requests
 bot = commands.Bot(command_prefix="£")
@@ -15,7 +15,13 @@ async def cat(context):
     embed = Embed()
     embed.set_image(url=requestJson[0]["url"])
     await context.send(embed=embed)
-    
 
+@bot.event
+async def on_message(summon):
+    await bot.process_commands(summon)
+    if bot.user.mentioned_in(summon):
+        embed = Embed(colour=Colour.from_rgb(193, 0, 238))
+        embed.set_image(url="https://i.imgflip.com/3ia3r2.png")
+        await summon.channel.send(embed=embed)
 
 bot.run(environ.get("COON_BOT"))
