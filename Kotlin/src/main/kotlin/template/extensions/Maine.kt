7d204d409064248d84be3ee6cb68fa8c8032cf67
Southpaw1496 <44805409+Southpaw1496@users.noreply.github.com>
2021-10-01 12:31:27 +0100

    First draft of hug command

--- Kotlin/src/main/kotlin/template/extensions/Maine.kt
+++ Kotlin/src/main/kotlin/template/extensions/Maine.kt
@@ -1,5 +1,8 @@
 package template.extensions
 
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalUser
+import com.kotlindiscord.kord.extensions.commands.converters.impl.user
 import com.kotlindiscord.kord.extensions.components.components
 import com.kotlindiscord.kord.extensions.components.publicButton
 import com.kotlindiscord.kord.extensions.components.types.emoji
@@ -9,6 +12,7 @@ import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.common.annotation.KordPreview
 import dev.kord.core.behavior.interaction.edit
 import dev.kord.core.entity.interaction.PublicFollowupMessage
+import dev.kord.rest.builder.message.create.allowedMentions
 import dev.kord.rest.builder.message.create.embed
 import dev.kord.rest.builder.message.modify.embed
 import template.CatAPI
@@ -64,7 +68,7 @@ class Maine : Extension() {
         }
         publicSlashCommand {
             name = "Pineapple"
-            description =  "Get a random pineapple from Unsplash"
+            description = "Get a random pineapple from Unsplash"
 
             guild(TEST_SERVER_ID)  // Otherwise it'll take an hour to update
 
@@ -76,7 +80,8 @@ class Maine : Extension() {
                     embed {
                         this.image = image.urls.regular
                     }
-                    this.content = "Photo by [${image.user.username}](${image.links.html}) on [Unsplash](https://unsplash.com)"
+                    this.content =
+                        "Photo by [${image.user.username}](${image.links.html}) on [Unsplash](https://unsplash.com)"
                     components(Duration.seconds(300)) {
                         onTimeout {
                             message.edit {
@@ -101,7 +106,7 @@ class Maine : Extension() {
         }
         publicSlashCommand {
             name = "Melon"
-            description =  "Get a random melon from Unsplash"
+            description = "Get a random melon from Unsplash"
 
             guild(TEST_SERVER_ID)  // Otherwise it'll take an hour to update
 
@@ -113,7 +118,8 @@ class Maine : Extension() {
                     embed {
                         this.image = image.urls.regular
                     }
-                    this.content = "Photo by [${image.user.username}](${image.links.html}) on [Unsplash](https://unsplash.com)"
+                    this.content =
+                        "Photo by [${image.user.username}](${image.links.html}) on [Unsplash](https://unsplash.com)"
                     components(Duration.seconds(300)) {
                         onTimeout {
                             message.edit {
@@ -136,5 +142,38 @@ class Maine : Extension() {
                 }
             }
         }
+        publicSlashCommand(::HugArgs)
+        {
+            name = "hug"
+            description = "Give someone a hug or receive one from Maine"
+
+            guild(TEST_SERVER_ID)  // Otherwise it'll take an hour to update
+            action {
+                respond {
+                    if(arguments.receiver == null || arguments.receiver == user) {
+                        content = "${user.mention} Have a special hug from me ❤️"
+                        embed {
+                            image = "https://c.tenor.com/eAKshP8ZYWAAAAAC/cat-love.gif"
+                        }
+
+                    } else {
+                        content = "${arguments.receiver!!.mention} Have a hug from ${user.mention}"
+                        embed {
+                            image = "https://media1.tenor.com/images/2d4138c7c24d21b9d17f66a54ee7ea03/tenor.gif?itemid=12535134)"
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    inner class HugArgs : Arguments() {
+        val receiver by optionalUser(
+            "receiver",
+            description = "Receiver of your hug",)
+
+
+
     }
 }
+
